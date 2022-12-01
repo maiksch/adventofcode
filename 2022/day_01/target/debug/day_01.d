a0bced201601ffd7203e7ac9e0e549b9f15ef5a6
Maik Schmaddebeck <maik.schmaddebeck@icloud.com>
2022-12-01 09:18:37 +0100

    gitignore

--- 2022/day_01/target/debug/day_01.d
+++
@@ -1,1 +0,0 @@
-/home/maik/personal/adventofcode/2022/day_01/target/debug/day_01: /home/maik/personal/adventofcode/2022/day_01/src/main.rs
--- 2022/day_01/target/debug/deps/day_01-be07331da699f476.d
+++
@@ -1,5 +0,0 @@
-/home/maik/personal/adventofcode/2022/day_01/target/debug/deps/day_01-be07331da699f476.rmeta: src/main.rs
-
-/home/maik/personal/adventofcode/2022/day_01/target/debug/deps/day_01-be07331da699f476.d: src/main.rs
-
-src/main.rs:
