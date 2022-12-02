1b2177a292729ce1ea18b9132925d7fc9251b794
Maik Schmaddebeck <maik.schmaddebeck@icloud.com>
2022-12-02 09:28:33 +0100

    Restructuring

--- 2022/src/bin/day_01.rs
+++ 2022/src/bin/day_01.rs
@@ -2,11 +2,11 @@ use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
 fn main() {
-    let file = File::open("input.txt").expect("Input could not be openend");
+    let file = File::open("input/day_01.txt").expect("Input could not be openend");
     let reader = BufReader::new(file);
 
-    let mut sum: Vec<usize> = Vec::new();
-    let mut calories_counter: usize = 0;
+    let mut sum = vec![];
+    let mut calories_counter = 0;
 
     for line in reader.lines().map(|l| l.unwrap()) {
         if line.is_empty() {
@@ -14,13 +14,15 @@ fn main() {
             calories_counter = 0;
             continue;
         }
-        let calories = usize::from_str_radix(&line, 10).expect("Calories were not a number");
+        let calories = str::parse::<usize>(&line).expect("Calories were not a number");
         calories_counter = calories_counter + calories;
     }
 
     sum.sort_by(|a, b| b.cmp(a));
-    let total: usize = (0..3).map(|i| sum[i]).sum();
 
     println!("{} are the highest calories carried", sum[0]);
+
+    let total = sum[0..3].iter().sum::<usize>();
+
     println!("{} calories are carried by the top three Elves", total);
 }
